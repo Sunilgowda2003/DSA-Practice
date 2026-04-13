accf875a1c8e30b1b29f0c47222e4bc79a9d799a
Sunil J S <suni84643@gmail.com>
2026-04-13 17:10:33 +0000

    Day 5 - Additional Star Pattern Programs

---
+++ Additionalstarpatterns/HallowTriangle.java
@@ -0,0 +1,31 @@
+package Additionalstarpatterns;
+
+public class HallowTriangle {
+    public static void main(String[] args) {
+        
+        System.out.println("Hallow Triangle...");
+        int n = 5;
+        // Outer loop -> rows
+        for(int i = 1;i <= n; i++){
+            // Inner loop -> columns
+            for(int j = 1; j <= i; j++){
+                // Print star only at borders
+                if(j == 1 || j == i || i == n){
+                    System.out.print("* ");//print star
+                }else{
+                    System.out.print("  ");//space inside
+                }
+
+            }
+            System.out.println();//next line
+
+        }
+    }
+    
+}
+//Uutput
+// * 
+// * * 
+// *   * 
+// *     * 
+// * * * * * 
