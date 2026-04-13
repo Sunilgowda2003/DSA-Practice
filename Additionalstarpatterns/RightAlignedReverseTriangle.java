d553e16118c71fb92adc1b69444a78e2e9c8bb66
Sunil J S <suni84643@gmail.com>
2026-04-13 17:27:46 +0000

    Day 5 - Additional Star Pattern Programs

---
+++ Additionalstarpatterns/RightAlignedReverseTriangle.java
@@ -0,0 +1,36 @@
+package Additionalstarpatterns;
+
+public class RightAlignedReverseTriangle {
+
+    public static void main(String[] args) {
+
+        System.out.println("Right Aligned Reverse Triangle...");
+
+        int n = 5;
+
+        // outer loop → rows
+        for(int i = n; i >= 1; i--){
+
+            // print spaces before stars
+            for(int j = 1; j <= n - i; j++){
+                System.out.print(" ");//space
+            }
+
+            // print stars
+            for(int k = 1; k <= i; k++){
+                
+                System.out.print("*"); 
+            }
+
+            // move to next line
+            System.out.println();
+        }
+    }
+}
+// Output:
+
+// *****
+//  ****
+//   ***
+//    **
+//     *
