414f74ece6623a2a121ca4369e3f959d7234e76d
Sunil J S <suni84643@gmail.com>
2026-04-15 18:08:52 +0000

    Day 7 - Number Alignment Patterns

---
+++ NumberAlignmentPatterns/SameNumberPyramid.java
@@ -0,0 +1,31 @@
+package NumberAlignmentPatterns;
+
+public class SameNumberPyramid {
+    public static void main(String[] args) {
+        System.out.println("Same Number Pyramid....");
+
+        int n = 5;
+        //rows
+        for(int i = 1;i <= n; i++){
+            //spaces
+            for(int j = 1;j <= n-i; j++){
+                System.out.print(" ");
+            }
+            //print same number in row
+            for(int k = 1;k <= i; k++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
+
+// Output:
+
+//     1 
+//    2 2 
+//   3 3 3 
+//  4 4 4 4 
+// 5 5 5 5 5  
+
