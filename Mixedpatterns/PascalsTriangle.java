e8cf38c7a1fa573173f8f5a7b2640906a1e15617
Sunil J S <suni84643@gmail.com>
2026-04-20 17:28:46 +0000

    Day 12 - Mixed Pattern Practice

---
+++ Mixedpatterns/PascalsTriangle.java
@@ -0,0 +1,37 @@
+package Mixedpatterns;
+
+public class PascalsTriangle {
+    public static void main(String[] args) {
+        System.out.println("Pascals Triangle......");
+
+        int n = 5;
+
+        // rows
+        for(int i = 0; i < n; i++) {
+
+            int num = 1;
+
+            // spaces
+            for(int j = 1; j < n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // numbers
+            for(int j = 0; j <= i; j++) {
+                System.out.print(num + " ");
+                num = num * (i - j) / (j + 1); // Pascal formula
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+// Output:
+
+//     1 
+//    1 1 
+//   1 2 1 
+//  1 3 3 1 
+// 1 4 6 4 1
+  
